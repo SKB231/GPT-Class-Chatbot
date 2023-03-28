f8fb44a38fef9a60297abe22ff435a40a5de56bd
Squidsword <messagemarvin@gmail.com>
2023-03-27 23:45:54 -0400

    Create an autocomplete function using regex

--- Swift_Backend/firebase.js
+++ Swift_Backend/firebase.js
@@ -27,19 +27,56 @@ async function getUserQueries(user) {
     return userQueries
 }
 
-async function deleteUserQueries(user) {
-    userQueries = await queries.where('user', '==', user).get() 
-    const batchSize = userQueries.size;
-    if (batchSize === 0) {
-      // when there are no documents left, we are done
-      resolve();
-      return;
+// async function deleteUserQueries(user) {
+//     userQueries = await queries.where('user', '==', user).get() 
+//     const batchSize = userQueries.size;
+//     if (batchSize === 0) {
+//       // when there are no documents left, we are done
+//       resolve();
+//       return;
+//     }
+//     const batch = db.batch();
+//     userQueries.docs.forEach((doc) => { // executes only after above line of code
+//       batch.delete(doc.ref);
+//     });
+//     await batch.commit();
+// }
+
+
+var data = require('./original_mock_data.json')
+
+function autocomplete(input, sort='length', num=20) {
+    if (data == null) {
+        return []
     }
-    const batch = db.batch();
-    userQueries.docs.forEach((doc) => { // executes only after above line of code
-      batch.delete(doc.ref);
-    });
-    await batch.commit();
+
+    var prefix = new RegExp(`^${input}`, 'i')
+    var pattern = new RegExp(`${input}`, 'i')
+    var prefix_matches = data.filter(d => d.question.search(prefix) >= 0)
+    top_suggestions = prefix_matches.sort((a, b) => custom_sort(a, b, sort))
+    if (top_suggestions.length >= num) {
+        return top_suggestions.splice(0, num)
+    }
+    var pattern_matches = data.filter(d => d.question.search(pattern) >= 0)
+    top_suggestions_2 = pattern_matches.sort((a, b) => custom_sort(a, b, sort))
+    top_suggestions = top_suggestions.concat(top_suggestions_2)
+    return top_suggestions.splice(0, num)
+}
+
+function custom_sort(a, b, sort='length') {
+    if (sort == 'length') {
+        weight = a["question"].length - b["question"].length
+        if (weight == 0) {
+            weight = (b["frequency"] - a["frequency"]) / 100
+        }
+        return weight
+    } else {
+        weight = (b["frequency"] - a["frequency"])
+        if (weight == 0) {
+            weight = (a["question"].length - b["question"].length) / 100
+        }
+        return weight
+    }
 }
 
 function delay(time) {
@@ -47,8 +84,7 @@ function delay(time) {
 }
 
 async function test() {
-  await delay(1000);
-  console.log(a);
+   console.log(autocomplete('what command'))
 }
 
 test();
