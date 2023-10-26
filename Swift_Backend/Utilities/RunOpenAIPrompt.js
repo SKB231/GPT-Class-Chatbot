e3cf0cb5b0638f52c843386767ac09291593370d
Shreekrishna Ravishankara Bhat <sbhat97@gatech.edu>
2023-10-26 13:57:36 -0400

    Added broiler plate code for Student class

--- Swift_Backend/Utilities/RunOpenAIPrompt.js
+++ Swift_Backend/Utilities/RunOpenAIPrompt.js
@@ -1,51 +1,113 @@
-const axios = require('axios');
-const url = 'https://api.openai.com/v1/chat/completions';
+const axios = require("axios");
+const url = "https://api.openai.com/v1/chat/completions";
 
 const OPENAI_API_KEY = `${process.env.OPENAI_API_KEY}`;
 
 const headers = {
-    'Authorization': `Bearer ${OPENAI_API_KEY}`,
-    'Content-Type': 'application/json'
-}
+    Authorization: `Bearer ${OPENAI_API_KEY}`,
+    "Content-Type": "application/json",
+};
 
-const instructions = 'You are a assistant, named Cosine, meant for students taking the Signal processing class of Georgia Tech.' + 
-                     'Students may converse with you or ask questions. This course is taught by James McClellan, and the course textbook is Signal Processing First by James McClellan.' +
-                     'Your first source for information should be this textbook.' +
-                     'If a user asks a shorter question, try to give a brief answer.' +
-                     'If a user asks a longer question, you can elaborate further, but try to focus on only what the user asks' +
-                     'If a user asks for you to elaborate, then feel free to elaborate' +
-                     'When answering users questions, if you can remember which chapter from Signal Processing First you got your answer from, then explicitly let the student know at the end of every response'
+const instructions =
+    "You are a assistant, named Cosine, meant for students of Georgia Tech.";
+
+const get_section = (sectionIndex) => {
+    console.log(sectionIndex);
+    console.log(`Returning the ${sectionIndex} section from the article!`);
+};
 
 class Chat {
     constructor() {
-      this.messages = [{"role": "system", "content": instructions}];
+        this.messages = [{ role: "system", content: instructions }];
     }
-  
+
     addMessage(role, message) {
-      this.messages.push({"role": role, "content": message });
+        this.messages.push({ role: role, content: message });
     }
 
     getTextResponse = async (callback) => {
-        axios.post(url, {
-            model: 'gpt-3.5-turbo',
-            // Messages should be an array of objects with 'role' and 'text' properties
-            messages: this.messages
-        },
-        {
-            headers: headers
-        }).then(response => {
-            console.log(response.data.choices[0].message)
-            let chatResponse = response.data.choices[0].message
-            this.addMessage(chatResponse.role, chatResponse.content)
-            callback(chatResponse.content)
-            return this.chatResponse
-        }).catch(error => {
-            console.log(error);
-        });
-    }
+        axios
+            .post(
+                url,
+                {
+                    model: "gpt-3.5-turbo",
+                    // Messages should be an array of objects with 'role' and 'text' properties
+                    messages: this.messages,
+                    functions: [
+                        {
+                            name: "get_section",
+                            description:
+                                "Given the section index, returns that section from the Stephen Wolfram's article",
+                            parameters: {
+                                type: "object",
+                                properties: {
+                                    index: {
+                                        type: "string",
+                                        description: "The section index",
+                                    },
+                                },
+                                required: ["index"],
+                            },
+                        },
+                    ],
+                },
+                {
+                    headers: headers,
+                }
+            )
+            .then((response) => {
+                let chatResponseContent = "";
+                const role = response.data.choices[0].message;
+                if (response.data.choices[0].finish_reason == "function_call") {
+                    // Function is called.
+                    const functionName =
+                        response.data.choices[0].message["function_call"][
+                            "name"
+                        ];
+                    if (
+                        response.data.choices[0].message &&
+                        response.data.choices[0].message?.content != ""
+                    ) {
+                        chatResponseContent =
+                            response.data.choices[0].message.content;
+                    } else {
+                        chatResponseContent = "Called function! ";
+                    }
+                    if (functionName == "get_section") {
+                        console.log(
+                            JSON.parse(
+                                response.data.choices[0].message[
+                                    "function_call"
+                                ]["arguments"]
+                            )["index"]
+                        );
+                        console.log(
+                            response.data.choices[0].message["function_call"]
+                        );
+
+                        get_section(
+                            JSON.parse(
+                                response.data.choices[0].message[
+                                    "function_call"
+                                ]["arguments"]
+                            )["index"]
+                        );
+                    }
+                } else {
+                    chatResponseContent = response.data.choices[0].message;
+                }
+
+                this.addMessage("GPT", chatResponseContent);
+                callback(chatResponseContent);
+                return chatResponseContent;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 }
-  
-module.exports = Chat
+
+module.exports = Chat;
 
 // let chat = new Chat()
 // chat.addMessage("user","Hi!")
---
+++ Swift_Backend/Utilities/Student.js
@@ -0,0 +1,26 @@
+class Student {
+    constructor(googleID) {
+        this.googleID = googleID
+        this.messages = []
+        this.topics = []// [(topicName, time), ]
+    }
+
+    function appendMessage(params) {
+        // adds 
+    }
+
+    function addTopic(topic, time) {
+
+    }
+
+    function serialize() {
+
+    }
+
+    function deserialize() {
+
+    }
+    
+
+
+}
